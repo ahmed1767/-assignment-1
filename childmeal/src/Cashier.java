8e92a574f8c564fc22eccaed8e00902504bb5c29
DESKTOP-VOQT66P\Ahmed Munna <munnaa329@gmail.com>
2018-04-16 21:21:17 +0600

    cashier_class_implemented(4th commit)

---
+++ childmeal/src/Cashier.java
@@ -0,0 +1,22 @@
+
+public class Cashier {
+	
+private Mealbuilder meal_builder;
+	
+	public Cashier(Mealbuilder meal_builder) {
+		this.meal_builder=meal_builder;
+	}
+	
+	public Meal get_meal() {
+		return this.meal_builder.get_meal();
+	}
+	
+	public void make_meal(String meal,String item,String drink,String toy) {
+		this.meal_builder.build_main_meal();
+		this.meal_builder.build_side_item();
+		this.meal_builder.build_drink();
+		this.meal_builder.build_toy();
+	}
+
+}
+
