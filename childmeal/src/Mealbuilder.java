092c876daac5997c8a18cdee0c8c2ad47bdb7cca
DESKTOP-VOQT66P\Ahmed Munna <munnaa329@gmail.com>
2018-04-16 21:15:21 +0600

    meal_builder_interface(2nd commit)

---
+++ childmeal/src/Mealbuilder.java
@@ -0,0 +1,7 @@
+public interface Mealbuilder {
+	public void build_main_meal(String meal);
+	public void build_side_item(String Item);
+	public void build_drink(String drink);
+	public void build_toy(String toy);
+	public Meal get_meal();
+}
